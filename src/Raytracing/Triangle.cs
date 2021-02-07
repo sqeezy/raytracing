93fd88489d653bc449277326d8c4558eac519d6d
Anton Herzog <info@antonherzog.com>
2021-02-07 18:59:08 +0100

    Refactor triangle.

--- src/Raytracing/Triangle.cs
+++ src/Raytracing/Triangle.cs
@@ -5,10 +5,10 @@ namespace Raytracing
 {
     public record Triangle(IReadOnlyList<Vector3> Points)
     {
-        public Vector3 A => Points[0];
-        public Vector3 B => Points[1];
-        public Vector3 C => Points[2];
+        public Vector3 A { get; } = Points[0];
+        public Vector3 B { get; } = Points[1];
+        public Vector3 C { get; } = Points[2];
 
-        public Vector3 Normal => Vector3.Cross(A, B);
+        public Vector3 Normal { get; } = Vector3.Cross(Points[0], Points[1]);
     }
 }
