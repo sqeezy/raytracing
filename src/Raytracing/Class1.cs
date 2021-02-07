6b43bde9ad05075e9d813013aa34247ea5358223
Anton Herzog <info@antonherzog.com>
2021-02-07 18:52:31 +0100

    Add triangle.

--- src/Raytracing/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Raytracing
-{
-    public class Class1
-    {
-    }
-}
---
+++ src/Raytracing/Triangle.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Numerics;
+
+namespace Raytracing
+{
+    public class Triangle
+    {
+        private readonly IReadOnlyList<Vector3> _points;
+
+        public Triangle(IReadOnlyList<Vector3> points)
+        {
+            _points = points;
+        }
+
+        public Vector3 A => _points[0];
+        public Vector3 B => _points[1];
+        public Vector3 C => _points[2];
+        public IReadOnlyList<Vector3> Points => _points;
+    }
+}
